c231481b4548c6db8412cd970aa2774fc53b1f9e
CosmicDan <dconnolly.au@gmail.com>
2016-12-17 08:46:21 +1100

    Add a couple helper scripts

---
+++ git_helpers/git_gui-Reign_src.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d "%~dp0\..\modpacks\Reign\src"
+git-gui
---
+++ git_helpers/git_pull-all.cmd
@@ -0,0 +1,13 @@
+@echo off
+cd /d "%~dp0\.."
+echo [#] Updating root...
+git pull
+echo.
+echo [#] Updating submodules...
+git submodule update --init --recursive
+cd modpacks\Reign\src
+git pull
+echo.
+echo Done. Press any key to close.
+pause > nul
+
