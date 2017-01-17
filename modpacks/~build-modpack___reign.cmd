b693469f703fd9b0db13e3dcead9cad6099fac85
CosmicDan <dconnolly.au@gmail.com>
2017-01-18 06:59:53 +1100

    Add modpack build script

---
+++ modpacks/~build-modpack___reign.cmd
@@ -0,0 +1,29 @@
+@echo off
+SETLOCAL
+for /f "tokens=1,2,3,4,5,6* delims=," %%i in ('%~dp0\date.exe +"%%Y,%%m,%%d,%%H,%%M,%%S"') do set yy=%%i& set mo=%%j& set dd=%%k& set hh=%%l& set mm=%%m& set ss=%%n
+SET VERSION=%yy%.%mo%.%dd%-%hh%.%mm%.%ss%
+SET MODPACK=Reign
+
+
+
+echo This script will build the %MODPACK% modpack from the src folder, ready for you to upload
+echo to the server.
+echo.
+echo Note that the old objects (previous modpack version) will be deleted - this is to save
+echo space on the server.
+echo.
+echo [?] Press any key to begin.
+pause > nul
+echo.
+echo [#] Deleting old objects...
+del /Q "%~dp0\%MODPACK%\distrib\%MODPACK%.json" >nul 2>&1
+rmdir /Q /S "%~dp0\%MODPACK%\distrib\objects" >nul 2>&1
+echo [#] Building new modpack...
+java -jar "%~dp0\launcher-builder-1.0.0-all.jar" --version "%VERSION%" --input "%~dp0\%MODPACK%" --output "%~dp0\%MODPACK%\distrib" --manifest-dest "%~dp0\%MODPACK%\distrib\%MODPACK%.json"
+
+echo.
+echo [?] Done! Press any key to close.
+pause
+
+:END
+ENDLOCAL
