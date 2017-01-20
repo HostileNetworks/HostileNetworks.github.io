9c3335c95d2659ebfb2aaeb3b84b2d874ebf2cf7
CosmicDan <dconnolly.au@gmail.com>
2017-01-20 17:52:15 +1100

    Don't include date.exe (GitHub complains)

--- modpacks/~build-modpack___reign.cmd
+++ modpacks/~build-modpack___reign.cmd
@@ -1,5 +1,13 @@
 @echo off
 SETLOCAL
+IF NOT EXIST "date.exe" (
+    echo [!] Error - 'date.exe' not found! Please get it from UnxUtils:
+    echo     https://sourceforge.net/projects/unxutils/files/unxutils/current/
+    echo.
+    echo [i] Press any key to exit.
+    pause>nul
+    GOTO :END
+)
 for /f "tokens=1,2,3,4,5,6* delims=," %%i in ('%~dp0\date.exe +"%%Y,%%m,%%d,%%H,%%M,%%S"') do set yy=%%i& set mo=%%j& set dd=%%k& set hh=%%l& set mm=%%m& set ss=%%n
 SET VERSION=%yy%.%mo%.%dd%-%hh%.%mm%.%ss%
 SET MODPACK=Reign
@@ -23,7 +31,7 @@ java -jar "%~dp0\launcher-builder-1.0.0-all.jar" --version "%VERSION%" --input "
 
 echo.
 echo [?] Done! Press any key to close.
-pause
+pause>nul
 
 :END
 ENDLOCAL
